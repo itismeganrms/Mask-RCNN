21c4a0546bbc70094e388d2842a74c3c83da9930
Megan Mirnalini Sundaram R <mrnalini@live.in>
2025-12-07 19:32:47 +0100

    Updating shell script for new dataset and runs

--- train.sh
+++ train.sh
@@ -2,10 +2,9 @@
 
 #SBATCH --job-name=maskrcnn
 #SBATCH --time=18:00:00
-#SBATCH --partition=gpu-long
-#SBATCH --output=/home/mrajaraman/slurm/maskrcnn/new-train/output-%A.out
+#SBATCH --partition=gpu-2080ti-11g
+#SBATCH --output=/home/mrajaraman/slurm/maskrcnn/merged-train/output-%A.out
 #SBATCH --gres=gpu:1
-#SBATCH --constraint="A100.4g.40gb|A100.3g.40gb"
 
 echo "## Starting GPU test on $HOSTNAME"
 module purge
@@ -22,27 +21,41 @@ pwd
 which conda
 python --version
 # pip freeze
-# LazyConfig Training Script - pretrained new baseline
+
+# LazyConfig Training Script - pretrained new baseline  
+# ---- TRAINING COMMANDS FOR ORIGINAL DATASET BELOW ----
 TILE_SIZE=512
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
-python /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/tools/lazyconfig_train_net.py --num-gpus 1 \
---config-file /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
---exp_id ${TILE_SIZE} \
---train_iter 5000 \
---dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
-train.output_dir=output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
-train.init_checkpoint="detectron2://ImageNetPretrained/torchvision/R-50.pkl" \
-dataloader.train.dataset.names=dragonfly_512_train \
-dataloader.test.dataset.names=dragonfly_512_valid \
-# dataloader.test.dataset.names=dragonfly_512_test \
+# python /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/tools/lazyconfig_train_net.py --num-gpus 1 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 5000 \
+# --dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
+# train.output_dir=output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# train.init_checkpoint="detectron2://ImageNetPretrained/torchvision/R-50.pkl" \
+# dataloader.train.dataset.names=dragonfly_512_train \
+# dataloader.test.dataset.names=dragonfly_512_valid \
+# # dataloader.test.dataset.names=dragonfly_512_test \
 
-python /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/tools/lazyconfig_train_net.py --num-gpus 1 \
---config-file /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
---exp_id ${TILE_SIZE} \
---train_iter 7500 \
---dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
-train.output_dir=output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
-train.init_checkpoint="detectron2://ImageNetPretrained/torchvision/R-50.pkl" \
-dataloader.train.dataset.names=dragonfly_512_train \
-dataloader.test.dataset.names=dragonfly_512_valid \
+# python /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/tools/lazyconfig_train_net.py --num-gpus 1 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 7500 \
+# --dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
+# train.output_dir=output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# train.init_checkpoint="detectron2://ImageNetPretrained/torchvision/R-50.pkl" \
+# dataloader.train.dataset.names=dragonfly_512_train \
+# dataloader.test.dataset.names=dragonfly_512_valid \
+
+# python /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/tools/lazyconfig_train_net.py --num-gpus 1 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/mask-rcnn-dragonfly/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 25000 \
+# --dataset_path /home/mrajaraman/dataset/coco-merged/ \
+# train.output_dir=output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# train.init_checkpoint="detectron2://ImageNetPretrained/torchvision/R-50.pkl" \
+# dataloader.train.dataset.names=dragonfly_512_train \
+# dataloader.test.dataset.names=dragonfly_512_valid 
+
+# ---- TRAINING COMMANDS FOR MERGED DATASET BELOW ----
